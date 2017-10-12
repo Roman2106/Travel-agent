b2577e255839064251862bcacb010d5c083450e0
Roman2106 <31575082+Roman2106@users.noreply.github.com>
2017-10-12 23:42:08 +0300

    first test

--- client/src/__tests__/FormForTrips-test.js
+++ client/src/__tests__/FormForTrips-test.js
@@ -1,19 +1,31 @@
 import React from "react";
-import { configure } from "enzyme";
+import {configure} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import TripsForm from "../components/Trips/FormForTrips";
-import { shallow, mount, render } from "enzyme";
+import {shallow, mount, render} from "enzyme";
 import {MemoryRouter} from "react-router-dom";
+import * as Api from "../api/api";
 
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
+
+const originalGetAll = Api.getAll;
+let locationsPromise;
+
+beforeEach(() => {
+  locationsPromise = Promise.resolve([]);
+  Api.getAll = () => locationsPromise;
+});
+
+afterEach(() => {
+  Api.getAll = originalGetAll;
+});
 
 it("Should call onAdd, by pressing the button", () => {
-  const onAdd = jest.fn();
-  const wrapper = shallow(<MemoryRouter><TripsForm onAdd={onAdd}/></MemoryRouter>);
-  // const button = wrapper.find("Loader");
-  // const button = tripsForm.find("button.addEditTrips");
-  expect(wrapper.find('TripsForm').exists()).toBe(true);
-  // console.log(wrapper.find("Loader"));
-  // button.simulate("click");
-  // expect(onAdd.mock.calls.length).toBe(1);
+  locationsPromise.then(locations => {
+    const addEdit = jest.fn();
+    const wrapper = mount(<MemoryRouter><TripsForm locations={locations} addEdit={addEdit}/></MemoryRouter>);
+    const button = wrapper.find(".addEditTrips");
+    button.simulate("click");
+    expect(addEdit.mock.calls.length).toBe(1);
+  });
 });
